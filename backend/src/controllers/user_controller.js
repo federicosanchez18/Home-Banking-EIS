eb725f0fd5ca52f90bb40ca35000010994782256
JonMaia <jonathannicolas.maia@gmail.com>
2019-11-04 14:04:16 -0300

    Se agregó la funcionalidad de logeo de un usuario con su respectiva ruta

--- backend/src/controllers/user_controller.js
+++ backend/src/controllers/user_controller.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const ErrorHandler = require('../errors_response/error_handler');
-//const ErrorToFindUser = require('../errors_response/error_to_find_user');
+const ErrorToFindUser = require('../errors_response/error_to_find_user');
+const ErrorPasswordInvalid = require('../errors_response/error_password_invalid');
 const ErrorValidation = require('../errors_response/error_validation');
 
 module.exports = class UserController {
@@ -10,6 +11,22 @@ module.exports = class UserController {
             const user = new User(req.body);
             await user.save();
             res.send(user);
+        } catch(error) {
+            return ErrorHandler.handleError(res, new ErrorValidation(error.message));
+        }
+    }
+
+    static async loginUser(req, res) {
+        try {
+            const user = await User.findOne({ email: req.body.email }).exec();
+            if(!user){
+                return ErrorHandler.handleError(res, new ErrorToFindUser());
+            }
+            if(!await user.comparePassword(req.body.password)){
+                return ErrorHandler.handleError(res, new ErrorPasswordInvalid());
+            } else {
+                res.json({message: `The user ${user.name} is login correctly`});
+            }
         } catch(error) {
             return ErrorHandler.handleError(res, new ErrorValidation(error.message));
         }
--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user_controller');
 
-router.route('/').post(UserController.registerUser);
+router.route('/register').post(UserController.registerUser);
+router.route('/login').post(UserController.loginUser);
 
 module.exports = router;
