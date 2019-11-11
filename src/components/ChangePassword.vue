9a18291b81e23eab8ad9716837e8cd79df8b01f6
ManuelMichellon <manuel.michellon@gmail.com>
2019-11-11 15:07:08 -0300

    cambio clave

--- src/components/ChangePassword.vue
+++ src/components/ChangePassword.vue
@@ -4,28 +4,28 @@
         <input type="text" name="userName" v-model="input.username" placeholder="UserName"/>
         <input type="text" name="password" v-model="input.password" placeholder="Password"/>
         <input type="text" name="newPassword" v-model="input.newPassword" placeholder="NewPassword"/>
-        <button type="button" v-on: click="changePassword()">Change Password</button>
+        <button type="button" v-on:click="changePassword()">Change Password</button>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'changePassword',
+    name:'changePassword',
     data(){
         return{
             input: {
-                username: "",
-                password: "",
-                newPassword: "",
+                username:"",
+                password:"",
+                newPassword:"",
             }
         }
     },
     methods: {
         changePassword() {
-            if(this.input.username != "" && this.input.password != "" && this.input.newPassword != ""){
+            if(this.input.username !="" && this.input.password !="" && this.input.newPassword !=""){
                 if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password){
-                    this.$parent.password = //llamar metodo en app que cambie la pass
-                    //sweet alert alert("")
+                    this.$parent.password =  this.input.newPassword,
+                    this.alert("Password change successfully"),
                     this.$router.replace({name: "Login"})
                 }else{
                     console.log('The username and / or password is incorrect')
