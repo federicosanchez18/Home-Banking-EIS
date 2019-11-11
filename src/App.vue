6085d42adac367b98a3fa6b30732f4e0eef3366e
unknown <fedeericosanchez18@gmail.com>
2019-11-11 10:21:48 -0300

    login

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,50 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div id="nav">
+      <router-link v-if="authenticated" to="/Login" v-on:click.native="logout()" replace>Logout</router-link>
+    </div>
+    <router-view @authenticated="setAuthenticated" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: 'fedeericosanchez18@gmail.com',
+        password: 'pepe'
+      }
+    }
+  },
+  mounted () {
+    if (!this.authenticated) {
+      this.$router.replace({ name: 'Login' })
+    }
+  },
+  methods: {
+    setAuthenticated (status) {
+      this.authenticated = status
+    },
+    logout () {
+      this.authenticated = false
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    background-color: #F0F0F0;
+  }
+  h1 {
+    padding: 0;
+    margin-top: 0;
+  }
+  #app {
+    width: 1024px;
+    margin: auto;
+  }
 </style>
