24a53f95789ae625a6f98c8985fdd7cb4340a8b8
JonMaia <jonathannicolas.maia@gmail.com>
2019-11-18 14:14:21 -0300

    Se implementó el uso de la librería cors para poder trabajar con la api del backend

--- backend/src/app_backend.js
+++ backend/src/app_backend.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
+
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin:"localhost:8080",
+                    credentials: true
+                }
+            ]
+        }
+    }
+}
 
 // Middlewares
 if (process.env.NODE_ENV !== 'test'){
     app.use(morgan('dev'));
 }
 app.use(express.json());
+app.use(cors(
+    config.application.cors.server
+));
 
 // Routes
 app.use('/user', require('./routes/user_routes'));
