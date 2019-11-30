dcf3d491051f93bb1131764330482107b1f705ba
JonMaia <jonathannicolas.maia@gmail.com>
2019-11-30 10:53:16 -0300

    Se implementó que el usuario contenga la información de los servicios que contiene y se hizo refactor de las rutas

--- backend/src/routes/services_routes.js
+++ backend/src/routes/services_routes.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const ServicesController = require('../controllers/services_controller');
 
-router.route('/create').post(ServicesController.createServices);
+//POST
+router.route('/create/:id').post(ServicesController.createServicesAndSend);
+//GET
+router.route('/:objectId').get(ServicesController.getServicesToObjectId);
+//PUT
 router.route('/pay/:paymentCode').put(ServicesController.payServices);
 router.route('/:paymentCode').put(ServicesController.updateServices);
+//DELETE
 router.route('/:paymentCode').delete(ServicesController.deleteServices);
 
 module.exports = router;
--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -2,15 +2,20 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user_controller');
 
+//POST
 router.route('/register').post(UserController.registerUser);
 router.route('/login').post(UserController.loginUser);
-router.route('/:id').put(UserController.updateUser);
-router.route('/:id').delete(UserController.deleteUser);
+//PUT
 router.route('/deposit/:id').put(UserController.toDepositAmount);
 router.route('/extraction/:id').put(UserController.toExtractAmount);
 router.route('/limit/:id').put(UserController.updateLimit);
 router.route('/transfer/:id').put(UserController.toTransfer);
-router.route('/:id').get(UserController.getUserCBU);
 router.route('/payservices/:id').put(UserController.paidServices);
+router.route('/services/:id').put(UserController.updateUserServices);
+//GET
+router.route('/cbu/:id').get(UserController.getUserCBU);
+router.route('/services/:id').get(UserController.getServices);
+//DELETE
+router.route('/delete/:id').delete(UserController.deleteUser);
 
 module.exports = router;
